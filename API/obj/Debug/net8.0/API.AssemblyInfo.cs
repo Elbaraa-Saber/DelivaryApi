1c43882af5ae2daff82fd4895bd0bf830198825f
Elbaraa <baraax01@gmail.com>
2025-10-26 02:34:42 +0700

    update gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf88705e3a6fcc1bf7a9078dcfce20f6500259af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ea892270f5083efb91935fe5e4f76ddd3f4336")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
