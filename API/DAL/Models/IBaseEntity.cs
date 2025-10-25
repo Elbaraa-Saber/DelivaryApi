bf88705e3a6fcc1bf7a9078dcfce20f6500259af
Elbaraa <baraax01@gmail.com>
2025-10-26 02:26:33 +0700

    Remove old migration to fix errors

--- API/DAL/Models/IBaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.DAL.Models
-{
-    public interface IBaseEntity
-    {
-        DateTime CreateDateTime { get; set; }
-        DateTime ModifyDateTime { get; set; }
-        DateTime? DeleteDateTime { get; set; }
-    }
-}
