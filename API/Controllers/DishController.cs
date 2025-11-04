6ffa1e2e9f95fbaebd8caefff423efa4a19a8986
Elbaraa-Saber <baraax01@gmail.com>
2025-11-05 04:13:17 +0700

    Created Controllers

---
+++ API/Controllers/DishController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DishController : ControllerBase
+    {
+        private readonly IDishService _dishService;
+
+        public DishController(IDishService dishService)
+        {
+            _dishService = dishService;
+        }
+
+        // GET /api/dish
+        [HttpGet]
+        public async Task<IActionResult> GetDishes([FromQuery] DishQueryParamsDto query)
+        {
+            var result = await _dishService.GetDishesAsync(query);
+            return Ok(result);
+        }
+
+        // GET /api/dish/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDishDetails(Guid id)
+        {
+            var result = await _dishService.GetDishDetailsAsync(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ API/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET /api/order
+        [HttpGet]
+        public async Task<IActionResult> GetOrders([FromQuery] OrderQueryParamsDto query)
+        {
+            var result = await _orderService.GetOrdersAsync(query);
+            return Ok(result);
+        }
+
+        // GET /api/order/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderDetails(Guid id)
+        {
+            var result = await _orderService.GetOrderDetailsAsync(id);
+            return Ok(result);
+        }
+    }
+}
