494c626bac91070d0ddb18890285cc0431d6e763
Elbaraa-Saber <baraax01@gmail.com>
2025-11-04 20:33:53 +0700

    Add DTOs for dishes, orders, and users

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e43f2b3f55299044d834f30d4159ed3a182e9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08682426640afdf9b1233d3cea2e90eba00931d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
