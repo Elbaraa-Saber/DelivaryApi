141765d508fba9dd3193acd39a6130b41105833d
Elbaraa-Saber <baraax01@gmail.com>
2025-11-07 19:33:29 +0700

    Add gender and phoneNumber to user

--- DataAccessLayer/Models/User.cs
+++ DataAccessLayer/Models/User.cs
@@ -7,6 +7,8 @@ namespace DataAccessLayer.Models
         public string FullName { get; set; }
         public DateTime BirthDate { get; set; }
         public string Address { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
 
         // from IBaseEntity
         public Guid Id { get; set; } 
